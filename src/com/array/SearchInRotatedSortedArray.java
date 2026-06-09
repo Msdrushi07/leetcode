b534e8389d2cf4ba579f77b1fce80f4b59be98ed
Msdrushi07 <108717120+Msdrushi07@users.noreply.github.com>
2026-06-09 10:40:23 +0530

    Add search method for rotated sorted array
    Implemented binary search for target in rotated sorted array.

--- src/com/array/SearchInRotatedSortedArray.java
+++ src/com/array/SearchInRotatedSortedArray.java
@@ -1,24 +1,47 @@
 package com.array;
 
-public class SearchInRotatedSortedArray {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int arr[] = {4,5,6,7,0,1,2};
-		int target = 0;
-		int start = 0;
-		int end = arr.length-1;
-		while(start < end) {
-			int mid = start + (end-start)/2;
-			for(int i=start;i<=mid;i++) {
-				if(arr[i] == target) {
-					System.out.println("element found at index :"+i);
-					break;
-				}
-			}
-		start = mid+1;
-		}
+public static int search(int[] nums, int target) {
 
-	}
+    int start = 0;
+    int end = nums.length - 1;
 
+    while (start <= end) {
+
+        int mid = start + (end - start) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        // Left half sorted
+        if (nums[start] <= nums[mid]) {
+
+            if (target >= nums[start] &&
+                target < nums[mid]) {
+
+                end = mid - 1;
+
+            } else {
+
+                start = mid + 1;
+            }
+
+        }
+        // Right half sorted
+        else {
+
+            if (target > nums[mid] &&
+                target <= nums[end]) {
+
+                start = mid + 1;
+
+            } else {
+
+                end = mid - 1;
+            }
+        }
+    }
+
+    return -1;
 }
