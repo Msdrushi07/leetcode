f32b2fffa9bbb180a10b4dbb552e270972f1d5bd
Msdrushi07 <108717120+Msdrushi07@users.noreply.github.com>
2026-06-09 09:53:38 +0530

    Refactor FindMinInRotatedSortedArray class

--- src/com/array/FindMinInRotatedSortedArray.java
+++ src/com/array/FindMinInRotatedSortedArray.java
@@ -1,27 +1,28 @@
-package com.array;
+public class MinimumInRotatedSortedArray {
 
-public class FindMinInRotatedSortedArray {
+    public static int findMin(int[] nums) {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int arr[] = {3,4,5,1,2};
-		int start = 0;
-		int end = arr.length-1;
-		int min = Integer.MAX_VALUE;
-		while(start < end) {
-			int mid = start + (end -start)/2;
-			if(arr[start] < arr[mid]) {
-				min = Math.min(min,arr[start]);
-				start = mid+1;
-			}
-			else {
-				min = Math.min(min, arr[mid]);
-				end = mid - 1;
-			}
-		}
-		
-		System.out.println(min);
+        int start = 0;
+        int end = nums.length - 1;
 
-	}
+        while(start < end){
 
+            int mid = start + (end - start)/2;
+
+            if(nums[mid] > nums[end]){
+                start = mid + 1;
+            }else{
+                end = mid;
+            }
+        }
+
+        return nums[start];
+    }
+
+    public static void main(String[] args) {
+
+        int nums[] = {3,4,5,1,2};
+
+        System.out.println(findMin(nums));
+    }
 }
